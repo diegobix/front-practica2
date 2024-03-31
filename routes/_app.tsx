cf98f7acef102bb58d181af29d80d290aabf5bfa
Smmook <diegobix97@gmail.com>
2024-03-31 15:35:23 +0200

    final tweaks

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,7 +5,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>practica2-2</title>
+        <title>Heroes</title>
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
